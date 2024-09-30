f5081b96cb6f1d400d9ba37694cbf8683b147aa4
Oleksii Kurin <olexkurin@gmail.com>
2024-09-30 23:18:10 +0300

    Add all exceptions processing

--- apiproject/apiapp/utils.py
+++ apiproject/apiapp/utils.py
@@ -1,12 +1,14 @@
 from rest_framework.views import exception_handler
 from rest_framework.exceptions import ValidationError
-from rest_framework.status import HTTP_422_UNPROCESSABLE_ENTITY
+from rest_framework import status
+from rest_framework.response import Response
 
 
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
-
-    if isinstance(exc, ValidationError) and response is not None:
-        response.status_code = HTTP_422_UNPROCESSABLE_ENTITY
+    if response is None:
+        response = Response({"detail": "Internal error occured."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    elif isinstance(exc, ValidationError):
+        response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
 
     return response
